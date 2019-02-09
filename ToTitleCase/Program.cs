fcd3a0d4dc1f8e9ddb7699e6b990b03df79b1461
Peter Billings <peterbillings7@gmail.com>
2019-02-09 10:47:48 -0600

    Adding VScode files for ToTitleCase

---
+++ ToTitleCase/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ToTitleCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            Console.Write("Please enter the string you wish to convert to Title Case\n\n>>");
+
+            input = Console.ReadLine();
+
+            titleCase tC = new titleCase(input);
+
+            Console.WriteLine(tC.makeTitleCase());
+        }
+    }
+}
---
+++ ToTitleCase/titleCase.cs
@@ -0,0 +1,41 @@
+using System;
+
+public class titleCase
+{
+    private string inputString;
+
+    public titleCase()
+    {
+        inputString = "";
+    }
+
+    public titleCase( string input )
+    {
+        inputString = input;
+    }
+
+    public string makeTitleCase()
+    {
+        string outputString = "";
+
+        string[] inputWords = inputString.Split(" ");
+
+        foreach (string word in inputWords)
+        {
+            outputString += word.Substring(0, 1).ToUpper();
+            outputString += word.Substring(1).ToLower() + " ";
+        }
+
+        return outputString;
+    }
+
+    public void setInputString(string newInputString)
+    {
+        inputString = newInputString;
+    }
+
+    public string getInputString()
+    {
+        return inputString;
+    }
+} 
