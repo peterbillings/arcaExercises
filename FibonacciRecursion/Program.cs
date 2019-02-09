4a4d06c9775c8eada459f442d3ae6a39b3d84310
Peter Billings <peterbillings7@gmail.com>
2019-02-09 10:19:58 -0600

    Adding the VScode files for FibonacciRecursion to repository

---
+++ FibonacciRecursion/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FibonacciRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter how manys numbers of the Fibonacci sequence you wish to see: ");
+
+            int fibonacciNumber = Convert.ToInt32(Console.ReadLine());
+
+            while (fibonacciNumber <= 0)
+            {
+                Console.Write("The number you entered is out of range, please enter a number greater than zero: ");
+
+                fibonacciNumber = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 1; i <= fibonacciNumber; i++)
+
+            {
+                Console.Write(Fibonacci(i) + " ");
+            }
+        }
+
+        public static int Fibonacci(int sequenceNumber)
+        {
+            if(sequenceNumber == 1 || sequenceNumber == 2)
+            {
+                return 1;
+            }
+
+          return Fibonacci(sequenceNumber - 2) + Fibonacci(sequenceNumber - 1);
+        }
+    }
+}
