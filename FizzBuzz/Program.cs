a72caab35ef5cf5f3373f76fd45820459c1d87dc
Peter Billings <peterbillings7@gmail.com>
2019-02-28 10:15:11 -0600

    Added FizzBizz program in C# to repo

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to FizzBuzz! Press ENTER to continue!");
+            Console.ReadLine();
+            Console.WriteLine();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 != 0 && i % 5 != 0)
+                {
+                    Console.Write(i);
+                }
+
+                if ( i % 3 == 0 )
+                {
+                    Console.Write("Fizz");
+                }
+                    
+                if ( i % 5 == 0 )
+                {
+                    Console.Write("Buzz");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
